da44a63d419ea0f0af98fb58714c142c09cd9195
KEN_2000 <79527273+ken-2000l@users.noreply.github.com>
2022-06-12 00:34:39 +0400

    Create file containing typing information about the data structures

---
+++ filemgr/types.py
@@ -0,0 +1,32 @@
+from time import struct_time
+from datetime import timedelta
+from typing import Optional, TypedDict
+
+
+class Track(TypedDict):
+    trackName: str
+    artistName: str
+    albumName: str
+    trackUri: str  # spotify:track:<trackID>
+
+
+class Item(TypedDict):
+    track: Track
+    episode: Optional[str]
+    localTrack: Optional[str]
+    addedDate: struct_time  # str  # yyyy-mm-dd
+
+
+class PlayList(TypedDict):
+    name: str
+    lastModifiedDate: struct_time  # str  # yyyy-mm-dd
+    items: list[Item]
+    description: Optional[str]
+    numberOfFollowers: int
+
+
+class History(TypedDict):
+    endTime: struct_time  # str  # yyyy-mm-dd HH:MM
+    artistName: str
+    trackName: str
+    msPlayed: timedelta  # int
