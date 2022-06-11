a84c9d16829300e82f5c6cee3ec1a3f7fddfc69f
KEN_2000 <79527273+ken-2000l@users.noreply.github.com>
2022-06-12 00:36:15 +0400

    cleanup

--- filemgr/functions.py
+++ filemgr/functions.py
@@ -1,19 +1,23 @@
-from typing import Optional
-from zipfile import ZipFile
 import os
+from zipfile import ZipFile
 
 from .data import MyData
 
 
-def _extract_data(path: str):
+def _extract_data(path: str) -> None:
     if not os.path.exists('MyData'):
         with ZipFile(path) as zip_:
+            # todo: check extracted location & modifiable root path
             zip_.extractall()
 
 
-def load_zipped_data(path: Optional[str] = None):
-    if path is None:
-        path = 'my_spotify_data.zip'
+def load_zipped_data(path: str = 'my_spotify_data.zip') -> MyData:
+    """
+    Loads a zipped Spotify Data Package into a MyData object and returns it.
+
+    :param path: relative path to zip file ('my_spotify_data.zip' default)
+    """
     _extract_data(path)
 
+    # todo: modifiable root path
     return MyData()
